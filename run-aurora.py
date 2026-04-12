0d5ba52bc29f2c982b620fee3dcdc23c701cec47
soCromp <cromp@wisc.edu>
2026-04-12 15:33:00 -0500

    latest

--- run-aurora.py
+++ run-aurora.py
@@ -8,6 +8,7 @@ from huggingface_hub import hf_hub_download
 import os 
 import numpy as np
 from tqdm import tqdm
+import pandas as pd
 
 in_path = '/mnt/data/sonia/aurora-data/date/input-natlantic-multivar-fullcontext/test'
 out_path = '/mnt/data/sonia/aurora-out/date/raw-natlantic-multivar-fullcontext/test'
@@ -19,6 +20,42 @@ u_channel = 1
 v_channel = 2
 t_channel = 3
 q_channel = 4
+
+#### Load tracks data
+trackspath1='/home/sonia/mcms/tracker/1940-2010/era5/out_era5/era5/mcms_era5_1940_2010_tracks.txt'
+trackspath2='/home/sonia/mcms/tracker/2010-2024/era5/out_era5/era5/FIXEDmcms_era5_2010_2024_tracks.txt'
+joinyear = 2010 # overlap for the track data
+start_year = 1940 #inclusive
+stop_year = 2024 #inclusive
+
+tracks1 = pd.read_csv(trackspath1, sep=' ', header=None, 
+        names=['year', 'month', 'day', 'hour', 'total_hrs', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5', 'unk6', 
+               'z1', 'z2', 'unk7', 'tid', 'sid'])
+# storms that start before the join year (even if they continue into the join year):
+sids1 = tracks1[(tracks1['sid']==tracks1['tid']) & (tracks1['year']<joinyear)]['sid'].unique()
+tracks1 = tracks1[tracks1['sid'].isin(sids1)]
+
+tracks2 = pd.read_csv(trackspath2, sep=' ', header=None, 
+        names=['year', 'month', 'day', 'hour', 'total_hrs', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5', 'unk6', 
+               'z1', 'z2', 'unk7', 'tid', 'sid'])
+# filter out storms that "start" at the beginning of the join year since they probably started before and are 
+# included in tracks1
+sids2 = tracks2[(tracks2['sid']==tracks2['tid']) & \
+        ((tracks2['year']>=joinyear) | (tracks2['month']>1) | (tracks2['day']>1) | (tracks2['hour']>0))]['sid'].unique()
+tracks2 = tracks2[tracks2['sid'].isin(sids2)]
+
+tracks = pd.concat([tracks1, tracks2], ignore_index=True)
+tracks = tracks[tracks['year']>=start_year]
+tracks = tracks.sort_values(by=['year', 'month', 'day', 'hour'])
+
+# conversions from the MCMS lat/lon system, as described in Jimmy's email:
+tracks['lat'] = 90-tracks['unk1'].values/100
+tracks['lon'] = tracks['unk2'].values/100
+
+tracks = tracks[['year', 'month', 'day', 'hour', 'tid', 'sid', 'lat', 'lon']]
+
+
+#### Setup
 
 # Initialize the model and load the weights from Hugging Face
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -34,7 +71,7 @@ lons = torch.linspace(0, 360, 1440 + 1)[:-1]
 print('Loading prompts...')
 prompts = []
 names = []
-for sid in tqdm(os.listdir(in_path)):
+for sid in tqdm(sorted(os.listdir(in_path))):
     prompts.append(np.load(os.path.join(in_path, sid)))
     names.append(sid.split('.')[0])
 prompts = np.stack(prompts, axis=0) # N V H W
@@ -45,14 +82,9 @@ print('Prompt loading complete')
 prompts[:, slp_channel, :, :] *= 100.0
 
 # pressure level configuration 
-prompts = np.expand_dims(prompts, 1).repeat(2, axis=1) # N P V H W (P==pressure level)
-# prompts[:, 1, slp_channel, :, :] = np.nan #. no pressure level 
-prompts[:, 1, u_channel, :, :] = np.nan # 500
-prompts[:, 1, v_channel, :, :] = np.nan # 500
-prompts[:, 0, t_channel, :, :] = np.nan # 925
-prompts[:, 1, q_channel, :, :] = np.nan # 500
+prompts = np.expand_dims(prompts, 1) # N P V H W (P==pressure level)
 
-prompts_ds = TensorDataset(torch.from_numpy(prompts).float())
+prompts_ds = TensorDataset(torch.from_numpy(prompts).float(), names)
 prompts_loader = DataLoader(prompts_ds, batch_size=batch_size, shuffle=False)
 
 # Download the official static variables directly from Microsoft's repo
@@ -64,15 +96,15 @@ static_path = hf_hub_download(
 with open(static_path, "rb") as f:
     official_static = pickle.load(f) # lsm, z, slt
 
-preds = []
 
-for batch_prompts in tqdm(prompts_loader, total=len(prompts_loader)):
-    batch_prompts = batch_prompts[0] # BS P V H W
-    batch_prompts = batch_prompts.cuda()
+for batch_prompts, batch_sids in tqdm(prompts_loader, total=len(prompts_loader)):
+    # prompts: BS P V H W
+    batch_prompts = batch_prompts.float().cuda()
     # 1. Surface Variables: Shape (Batch, Time, Lat, Lon)
     # You are only using Temperature 2m ("2t")
     surf_vars = {
-        "msl": batch_prompts[:, 0, slp_channel, :, :].unsqueeze(1) # add in a time dimension, get rid of pressure dim
+        "msl": batch_prompts[:, 0, slp_channel, :, :].unsqueeze(1), # add in a time dimension, get rid of pressure dim
+        "2t":  batch_prompts[:, 0, t_channel, :, :].unsqueeze(1), # pass 925 as surface
     }
 
     # 2. Atmospheric Variables: Shape (Batch, Time, Levels, Lat, Lon)
@@ -81,7 +113,7 @@ for batch_prompts in tqdm(prompts_loader, total=len(prompts_loader)):
     atmos_vars = {
         "u": batch_prompts[:, :, u_channel, :, :].unsqueeze(1), # U-wind (primarily 500)
         "v": batch_prompts[:, :, v_channel, :, :].unsqueeze(1), # V-wind (primarily 500)
-        "t": batch_prompts[:, :, t_channel, :, :].unsqueeze(1), # Temperature (primarily 925)
+        # "t": batch_prompts[:, :, t_channel, :, :].unsqueeze(1), # Temperature (primarily 925)
         "q": batch_prompts[:, :, q_channel, :, :].unsqueeze(1), # Humidity (primarily 500)
     }
 
@@ -92,6 +124,9 @@ for batch_prompts in tqdm(prompts_loader, total=len(prompts_loader)):
         "lsm": torch.as_tensor(official_static["lsm"]),   
         "slt": torch.as_tensor(official_static["slt"])
     }
+    
+    rows = [tracks[tracks['tid']==int(sid)].to_dict(orient='records')[0] for sid in batch_sids]
+    dates = [datetime(row['year'], row['month'], row['day'], row['hour'], 0) for row in rows]
 
     # 4. Pack into the Aurora Batch object -- UNNORMALIZED!
     batch = Batch(
@@ -101,8 +136,8 @@ for batch_prompts in tqdm(prompts_loader, total=len(prompts_loader)):
         metadata=Metadata(
             lat=lats,
             lon=lons,
-            time=(datetime(2020, 6, 1, 12, 0),), # Start time of the storm
-            atmos_levels=(500, 925), # Explicitly define your two pressure levels
+            time=dates,
+            atmos_levels=(500,), 
         ),
     ).to(device)
 
@@ -113,28 +148,36 @@ for batch_prompts in tqdm(prompts_loader, total=len(prompts_loader)):
         # We move predictions to CPU immediately to prevent GPU memory overflow during rollout
         batch_preds = [pred.to("cpu") for pred in rollout(model, batch, steps=timesteps)]
         
-    batch_preds = [pred.unnormalise(model.surf_stats) for pred in batch_preds]
-    slp_pred = torch.cat([t.surf_vars['msl'] for t in batch_preds], dim=1).squeeze() # B T H W
+    # batch_preds = [pred.unnormalise(model.surf_stats) for pred in batch_preds]
+    slp_pred = torch.cat([t.surf_vars['msl'] for t in batch_preds], dim=1) # B T H W
     u_pred = torch.cat([t.atmos_vars['u'] for t in batch_preds], dim=1)[:, :, 0, :, :] # get rid of pressure lvl
     v_pred = torch.cat([t.atmos_vars['v'] for t in batch_preds], dim=1)[:, :, 0, :, :]
-    t_pred = torch.cat([t.atmos_vars['t'] for t in batch_preds], dim=1)[:, :, 1, :, :]
+    t_pred = torch.cat([t.surf_vars['2t'] for t in batch_preds], dim=1)
     q_pred = torch.cat([t.atmos_vars['q'] for t in batch_preds], dim=1)[:, :, 0, :, :]
     
-    preds.append(torch.stack([slp_pred, u_pred, v_pred, t_pred, q_pred], dim=2)) # appending: B T V H W
+    batch_preds = torch.stack([slp_pred, u_pred, v_pred, t_pred, q_pred], dim=2).float() # B T V H W
+    batch_preds[:, :, slp_channel, :, :] /= 100.0
+    batch_preds = batch_preds.permute(0, 1, 3, 4, 2) # N T H W V
+    batch_preds = batch_preds.numpy()
+    # print(batch_preds)
+    for i, sid in enumerate(batch_sids):
+        os.makedirs(os.path.join(out_path, sid), exist_ok=True)
+        for t in range(timesteps):
+            np.save(os.path.join(out_path, sid, f'{t}'), batch_preds[i, t])
     
     
-preds = torch.cat(preds) # N T V H W
-print(preds.shape)
+# preds = torch.cat(preds) # N T V H W
+# print(preds.shape)
 
-# slp from hpa to pa
-preds[:, :, slp_channel, :, :] /= 100.0
-preds = preds.permute(0, 1, 3, 4, 2) # N T H W V
-print(preds.shape)
-preds = preds.numpy()
+# # slp from hpa to pa
+# preds[:, :, slp_channel, :, :] /= 100.0
+# preds = preds.permute(0, 1, 3, 4, 2) # N T H W V
+# print(preds.shape)
+# preds = preds.numpy()
 
-print('Writing out predictions...')
-for i, name in enumerate(names):
-    os.makedirs(os.path.join(out_path, name), exist_ok=True)
-    for t in range(timesteps):
-        np.save(os.path.join(out_path, name, f'{t}'), preds[i, t])
-print('Prediction writing complete')
+# print('Writing out predictions...')
+# for i, name in enumerate(names):
+#     os.makedirs(os.path.join(out_path, name), exist_ok=True)
+#     for t in range(timesteps):
+#         np.save(os.path.join(out_path, name, f'{t}'), preds[i, t])
+# print('Prediction writing complete')
